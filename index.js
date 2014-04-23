a2809fddbbff6afcfc6898e21407d15a10dd7837
Mike Gioia <mikegioia@gmail.com>
2014-04-22 20:12:04 -0400

    adding all files

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * Base Interface class
+ */
+
+var Stapes = require( 'stapes' );
+
+// base model class subclasses stapes object
+var Interface = Stapes.subclass({
+
+    constructor: function () {}
+
+});
+
+module.exports = Interface;
