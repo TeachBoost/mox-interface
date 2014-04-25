827c1dc7eb7a9f4a0dff213f64cbf3f28cbbdbf8
Mike Gioia <mikegioia@gmail.com>
2014-04-25 12:40:44 -0400

    updated tests, package json

---
+++ test/interface.js
@@ -0,0 +1,27 @@
+/**
+ * Test the interface module
+ */
+
+var chai = require( 'chai' ),
+    expect = chai.expect,
+    Interface = require( '../' );
+
+describe( 'Interface', function () {
+
+    it( 'interface should be a function', function () {
+        expect( Interface )
+            .to.be.a( 'function' );
+    });
+
+    it( 'should construct a class property', function () {
+        var Subclass = Interface.subclass({
+            constructor: function ( bar ) {
+                this.bar = bar;
+            }
+        });
+        var Foo = new Subclass( 5 );
+        expect( Foo.bar )
+            .to.be.equal( 5 );
+    });
+
+});
